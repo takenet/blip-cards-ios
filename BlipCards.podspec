68526191232b29a220a0682e16bcf926ce56541c
Victor Balbo <victorbalbo@gmail.com>
2017-10-26 13:56:30 -0200

    Fix .podspec file

--- BlipCards.podspec
+++ BlipCards.podspec
@@ -30,7 +30,7 @@ TODO: Add long description of the pod here.
 
   s.ios.deployment_target = '8.0'
 
-  s.source_files = 'src/Blip/Blip.h'
+  s.source_files = 'src/Blip/**/*'
   
   # s.resource_bundles = {
   #   'BlipCards' => ['BlipCards/Assets/*.png']
--- src/Blip/Blip.h
+++ src/Blip/Blip.h
@@ -17,20 +17,3 @@ FOUNDATION_EXPORT const unsigned char BlipVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Blip/PublicHeader.h>
 
-#import <Blip/BlipCard.h>
-#import <Blip/BlipTapGestureRecognizer.h>
-#import <Blip/Builder.h>
-#import <Blip/ChatState.h>
-#import <Blip/HTMLMetaTag.h>
-#import <Blip/MediaAudio.h>
-#import <Blip/MediaDocument.h>
-#import <Blip/MediaDocumentShow.h>
-#import <Blip/MediaPicture.h>
-#import <Blip/MediaPictureShow.h>
-#import <Blip/MediaVideo.h>
-#import <Blip/MenuMultimedia.h>
-#import <Blip/QuickReply.h>
-#import <Blip/QuickReplyOptionCollectionViewCell.h>
-#import <Blip/Text.h>
-#import <Blip/UIImage+animatedGif.h>
-#import <Blip/WebLink.h>
